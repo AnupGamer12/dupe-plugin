a33e932a699a0f08144b8c8de3ad2c1ea7f57ecf
AnupGamer12 <97493797+AnupGamer12@users.noreply.github.com>
2024-07-24 21:48:18 +0545

    Create TotCommand.java

---
+++ src/main/java/me/anup/dupe/TotCommand.java
@@ -0,0 +1,24 @@
+package me.anup.dupe;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+public class TotCommands implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender,Command cmd,String label,  String[] args) {
+        if (sender instanceof Player){
+            Player player = (Player) sender;
+        }
+        if (cmd.getName().equalsIgnoreCase("dupe")){
+            if (sender instanceof Player){
+                Player player = (Player) sender;
+                player.getInventory().addItem(player.getItemInHand());
+            }
+        }
+        return true;
+    }
+}
